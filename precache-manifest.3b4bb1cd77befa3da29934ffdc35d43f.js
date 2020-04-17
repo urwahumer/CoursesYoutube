1875433dd44c8f78f6487fd1dde76dec85500d98
Hafiz Muhammad Urwah-External <muhammad.urwah@systemsltd.com>
2020-04-18 04:16:22 +0500

    Updates

---
+++ precache-manifest.3b4bb1cd77befa3da29934ffdc35d43f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1512b879e71c25f742ce15a7c36caa",
+    "url": "/CoursesYoutube/index.html"
+  },
+  {
+    "revision": "00289962bb8d896b7e72",
+    "url": "/CoursesYoutube/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1b8ff3636e5aca80fc02",
+    "url": "/CoursesYoutube/static/css/main.6bbc7c50.chunk.css"
+  },
+  {
+    "revision": "00289962bb8d896b7e72",
+    "url": "/CoursesYoutube/static/js/2.e783d742.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CoursesYoutube/static/js/2.e783d742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8ff3636e5aca80fc02",
+    "url": "/CoursesYoutube/static/js/main.e3a02337.chunk.js"
+  },
+  {
+    "revision": "24976f61c66f27736432",
+    "url": "/CoursesYoutube/static/js/runtime-main.16734924.js"
+  }
+]);
