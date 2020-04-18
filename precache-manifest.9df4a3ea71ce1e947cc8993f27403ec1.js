5ce4a33c211872caa9774cab55c40b180cfb80e4
Hafiz Muhammad Urwah-External <muhammad.urwah@systemsltd.com>
2020-04-18 20:10:28 +0500

    Updates

---
+++ precache-manifest.9df4a3ea71ce1e947cc8993f27403ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e17266d478bedf0f195b2fbab7bf26",
+    "url": "/CoursesYoutube/index.html"
+  },
+  {
+    "revision": "00289962bb8d896b7e72",
+    "url": "/CoursesYoutube/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "53a1515a073e6384539e",
+    "url": "/CoursesYoutube/static/css/main.6bbc7c50.chunk.css"
+  },
+  {
+    "revision": "00289962bb8d896b7e72",
+    "url": "/CoursesYoutube/static/js/2.e783d742.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CoursesYoutube/static/js/2.e783d742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a1515a073e6384539e",
+    "url": "/CoursesYoutube/static/js/main.b10d6bd7.chunk.js"
+  },
+  {
+    "revision": "24976f61c66f27736432",
+    "url": "/CoursesYoutube/static/js/runtime-main.16734924.js"
+  }
+]);
--- src/Components/ContactUs.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ContactUS = () => {
-  return <div></div>;
-};
-
-export default ContactUS;
